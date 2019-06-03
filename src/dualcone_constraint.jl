7547813adfb5762f8a0bd6b24d62b3f62ae973d4
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-06-03 10:49:11 -0300

    fix dual sets

--- src/dualcone_constraint.jl
+++ src/dualcone_constraint.jl
@@ -1,11 +1,11 @@
 function add_dualcone_cosntraint!(dualmodel::MOI.ModelLike, vi::VI,
                                   ::Type{SAF{T}}, ::Type{MOI.GreaterThan{T}}) where T
-    return MOI.add_constraint(dualmodel, SVF(vi), MOI.LessThan(0.0))
+    return MOI.add_constraint(dualmodel, SVF(vi), MOI.GreaterThan(0.0))
 end
 
 function add_dualcone_cosntraint!(dualmodel::MOI.ModelLike, vi::VI,
                                   ::Type{SAF{T}}, ::Type{MOI.LessThan{T}}) where T
-    return MOI.add_constraint(dualmodel, SVF(vi), MOI.GreaterThan(0.0))
+    return MOI.add_constraint(dualmodel, SVF(vi), MOI.LessThan(0.0))
 end
 
 function add_dualcone_cosntraint!(dualmodel::MOI.ModelLike, vi::VI,
