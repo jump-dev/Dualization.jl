a2ec51f2d8023f9a7af6ba7ce30aba79afdf1da4
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-06-26 13:36:46 -0300

    fixed tests

--- test/Solvers/clp_test.jl
+++ test/Solvers/clp_test.jl
@@ -9,8 +9,12 @@ const CLPOPT = with_optimizer(Clp.Optimizer, LogLevel = 0)
         lp4_test,
         lp5_test,
         lp6_test,
-        lp7_test
-        #lp8_test Int64 problem, does not work
+        lp7_test,
+        # lp8_test, Int64 problem, does not work
+        # lp9_test, Interval not supported
+        lp10_test,
+        # lp11_test, Feasibility not supported
+        lp12_test,
     ]
     test_strong_duality(list_of_linear_problems, CLPOPT)
 end
