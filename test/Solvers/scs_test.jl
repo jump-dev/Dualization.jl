82a1a2e50627f718d7d42f7ee3ec08e5394a8418
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-07-01 12:12:30 -0300

    added SCS to tests

---
+++ test/Solvers/scs_test.jl
@@ -0,0 +1,14 @@
+using SCS
+const SCSOPT = with_optimizer(SCS.Optimizer)
+
+@testset "SCS SOC Problems" begin
+    list_of_linear_problems = [     
+        soc1_test,
+        soc2_test,
+        soc3_test,
+        soc4_test,
+        soc5_test,
+        soc6_test
+    ]
+    test_strong_duality(list_of_linear_problems, SCSOPT)
+end
