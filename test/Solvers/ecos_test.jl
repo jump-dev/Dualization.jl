c1c37f700e48754cb545838816915f3d5541231e
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-07-09 15:05:27 -0300

    added conic linear ecos test

--- test/Solvers/ecos_test.jl
+++ test/Solvers/ecos_test.jl
@@ -2,6 +2,15 @@ using ECOS
 const ECOSOPT = with_optimizer(ECOS.Optimizer, verbose = 0)
 
 @testset "ECOS conic Problems" begin
+    @testset "ECOS Conic linear problems" begin
+        list_of_soc_problems = [     
+            conic_linear1_test,
+            conic_linear2_test,
+            conic_linear3_test,
+            conic_linear4_test
+        ]
+        test_strong_duality(list_of_soc_problems, ECOSOPT)
+    end
     @testset "ECOS SOC problems" begin
         list_of_soc_problems = [     
             soc1_test,
