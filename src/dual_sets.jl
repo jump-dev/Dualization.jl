cad1a0724c503b05b9faf5039b7688a99bc0aad6
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-07-17 11:45:13 -0300

    use copy instead of reinstantiate

--- src/dual_sets.jl
+++ src/dual_sets.jl
@@ -17,11 +17,11 @@ function dual_set(s::MOI.EqualTo{T}) where T
 end
 
 function dual_set(s::MOI.Nonpositives)
-    return MOI.Nonpositives(MOI.dimension(s))
+    return copy(s) # The set is self-dual
 end
 
 function dual_set(s::MOI.Nonnegatives)
-    return MOI.Nonnegatives(MOI.dimension(s))
+    return copy(s) # The set is self-dual
 end
 
 function dual_set(s::MOI.Zeros)
@@ -29,13 +29,13 @@ function dual_set(s::MOI.Zeros)
 end
 
 function dual_set(s::MOI.SecondOrderCone)
-    return MOI.SecondOrderCone(MOI.dimension(s))
+    return copy(s) # The set is self-dual
 end
 
 function dual_set(s::MOI.RotatedSecondOrderCone)
-    return MOI.RotatedSecondOrderCone(MOI.dimension(s))
+    return copy(s) # The set is self-dual
 end
 
 function dual_set(s::MOI.PositiveSemidefiniteConeTriangle)
-    return MOI.PositiveSemidefiniteConeTriangle(s.side_dimension) # Change to MOI.side_dimension in MOI 0.9.0
+    return copy(s) # The set is self-dual
 end
