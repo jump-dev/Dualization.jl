912cdf5a6763dd0c916098f2234876b719e54193
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-07-26 20:54:32 -0300

    added more tests

--- test/Tests/test_dual_model_variables.jl
+++ test/Tests/test_dual_model_variables.jl
@@ -1,12 +1,22 @@
+# Tests of this file not covered in other tests
 @testset "dual_model_variables.jl" begin
 
-    # Changed method
-    # @testset "push_to_dual_obj_aff_terms!" begin
-    #     dual_obj_affine_terms = Dict{VI, Float64}()
-    #     Dualization.push_to_dual_obj_aff_terms!(dual_obj_affine_terms, VI(1), 0.0)
-    #     @test isempty(dual_obj_affine_terms)
-    #     Dualization.push_to_dual_obj_aff_terms!(dual_obj_affine_terms, VI(1), 1.0)
-    #     @test !isempty(dual_obj_affine_terms)
-    # end
+    @testset "push_to_dual_obj_aff_terms!" begin
+        primal_model = soc1_test()
+        dual_obj_affine_terms = Dict{VI, Float64}()
+        ci = CI{VVF, MOI.SecondOrderCone}(2)
+        Dualization.push_to_dual_obj_aff_terms!(primal_model, dual_obj_affine_terms, VI(1), ci, 1)
+        @test isempty(dual_obj_affine_terms)
+    end
 
+    @testset "set_dual_variable_name" begin
+        primal_model = soc1_test()
+        vi = VI(1)
+        Dualization.set_dual_variable_name(primal_model, vi, 1, "con", "")
+        @test MOI.get(primal_model, MOI.VariableName(), vi) == "con_1"
+        Dualization.set_dual_variable_name(primal_model, vi, 2, "con", "")
+        @test MOI.get(primal_model, MOI.VariableName(), vi) == "con_2"
+        Dualization.set_dual_variable_name(primal_model, vi, 2, "con", "oi")
+        @test MOI.get(primal_model, MOI.VariableName(), vi) == "oicon_2"
+    end
 end
