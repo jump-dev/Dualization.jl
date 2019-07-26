fa0f5e2173bc78d3a5aeb6d003cf82f35e6e92bc
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-07-26 20:34:25 -0300

    removed lines and improved comments

--- src/dual_model_variables.jl
+++ src/dual_model_variables.jl
@@ -16,7 +16,7 @@ function add_dual_vars_in_dual_cones(dual_model::MOI.ModelLike, primal_model::MO
     return dual_obj_affine_terms
 end
 
-# Utils for primal_con_constants
+# Utils for primal_con_constants dict 
 function push_to_primal_con_constants!(primal_model::MOI.ModelLike, primal_con_constants::Dict{CI, Vector{T}}, 
                                       ci::CI{F, S}) where {T, F <: MOI.AbstractScalarFunction, S <: MOI.AbstractScalarSet}
     push!(primal_con_constants, ci => get_scalar_term(primal_model, ci))
@@ -28,7 +28,7 @@ function push_to_primal_con_constants!(primal_model::MOI.ModelLike, primal_con_c
     return # No constants need to be passed to the DualOptimizer in this case, don't need to push zero to the dict
 end
 
-# Utils for primal_con_dual_con
+# Utils for primal_con_dual_con dict
 function push_to_primal_con_dual_con!(primal_con_dual_con::Dict{CI, CI}, ci::CI, ci_dual::CI)
     push!(primal_con_dual_con, ci => ci_dual)
     return 
@@ -38,7 +38,7 @@ function push_to_primal_con_dual_con!(primal_con_dual_con::Dict{CI, CI}, ci::CI,
     return # Don't put in the dict a nothing value
 end
 
-# Utils for add_dual_variable functions
+# Utils for dual_obj_affine_terms dict
 function push_to_dual_obj_aff_terms!(primal_model::MOI.ModelLike, dual_obj_affine_terms::Dict{VI, T}, vi::VI,
                                      ci::CI{F, S}, i::Int) where {T, F <: MOI.AbstractFunction, S <: MOI.AbstractSet}
     value = get_scalar_term(primal_model, i, ci)
@@ -58,10 +58,9 @@ function _add_dual_variable(dual_model::MOI.ModelLike, primal_model::MOI.ModelLi
                             ci::CI{F, S}) where {T, F <: MOI.AbstractFunction, 
                                                     S <: MOI.AbstractSet}
 
-    row_dimension = get_ci_row_dimension(primal_model, ci) 
     # Change to add_constrained_varibales in MOI 0.9.0
     # because of https://github.com/guilhermebodin/Dualization.jl/issues/9
-    vis = MOI.add_variables(dual_model, row_dimension) # Add as many variables as the dimension of the constraint
+    vis = MOI.add_variables(dual_model, get_ci_row_dimension(primal_model, ci)) # Add as many variables as the dimension of the constraint
     push!(primal_con_dual_var, ci => vis) # Add the map of the added dual variable to the relationated constraint
     # Get constraint name
     ci_name = MOI.get(primal_model, MOI.ConstraintName(), ci)
@@ -69,11 +68,10 @@ function _add_dual_variable(dual_model::MOI.ModelLike, primal_model::MOI.ModelLi
     for (i, vi) in enumerate(vis)
         push_to_dual_obj_aff_terms!(primal_model, dual_obj_affine_terms, vi, ci, i)
         set_dual_variable_name(dual_model, vi, i, ci_name, 
-                                dual_names.dual_variable_name_prefix)
+                               dual_names.dual_variable_name_prefix)
     end
     return vis
 end
-
 
 function add_dual_variable(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, dual_names::DualNames,
                            primal_con_dual_var::Dict{CI, Vector{VI}}, dual_obj_affine_terms::Dict{VI, T},
@@ -84,7 +82,6 @@ function add_dual_variable(dual_model::MOI.ModelLike, primal_model::MOI.ModelLik
                              primal_con_dual_var, dual_obj_affine_terms, ci)
     return add_dual_cone_constraint(dual_model, primal_model, vis, ci)
 end
-
 
 function set_dual_variable_name(dual_model::MOI.ModelLike, vi::VI, i::Int, ci_name::String, prefix::String)
     MOI.set(dual_model, MOI.VariableName(), vi, prefix*ci_name*"_$i")
