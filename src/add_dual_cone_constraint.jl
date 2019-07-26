d1730a12cb7aa67805253bda612446945e73026a
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-07-26 20:34:01 -0300

    remove unecessary function

--- src/add_dual_cone_constraint.jl
+++ src/add_dual_cone_constraint.jl
@@ -1,26 +1,20 @@
-function add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vis::Vector{VI},
-                                  ci::CI{F, S}) where {F <: MOI.AbstractFunction, S <: MOI.AbstractSet}
-
-    return _add_dual_cone_constraint(dual_model, primal_model, vis, ci)
-end
-
-function _add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vi::Vector{VI},
+function add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vi::Vector{VI},
                                   ci::CI{F, S}) where {F <: MOI.AbstractScalarFunction, S <: MOI.AbstractScalarSet}
     # In this case vi should have only one entry
     return MOI.add_constraint(dual_model, SVF(vi[1]), dual_set(get_set(primal_model, ci)))
 end
 
-function _add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vi::Vector{VI},
+function add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vi::Vector{VI},
                                   ci::CI{F, MOI.EqualTo{T}}) where {T, F <: MOI.AbstractScalarFunction}
     return 
 end
 
-function _add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vis::Vector{VI},
+function add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vis::Vector{VI},
                                   ci::CI{F, S}) where {F <: MOI.AbstractVectorFunction, S <: MOI.AbstractVectorSet}
     return MOI.add_constraint(dual_model, VVF(vis), dual_set(get_set(primal_model, ci)))
 end
 
-function _add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vis::Vector{VI},
+function add_dual_cone_constraint(dual_model::MOI.ModelLike, primal_model::MOI.ModelLike, vis::Vector{VI},
                                   ci::CI{F, MOI.Zeros}) where {F <: MOI.AbstractVectorFunction}
     return 
 end
