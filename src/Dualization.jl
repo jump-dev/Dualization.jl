24b1234ce658c13b848ff36e8a37a21573e124bb
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-07-31 14:22:46 -0300

    optimizer working

--- src/Dualization.jl
+++ src/Dualization.jl
@@ -27,7 +27,7 @@ MOIU.@model(DualizableModel,
             (MOI.VectorAffineFunction,))
 
 
-struct PrimalDualMap{T}
+mutable struct PrimalDualMap{T}
     primal_var_dual_con::Dict{VI, CI}
     primal_con_dual_var::Dict{CI, Vector{VI}}
     primal_con_dual_con::Dict{CI, CI}
@@ -35,15 +35,43 @@ struct PrimalDualMap{T}
 
     function PrimalDualMap{T}() where T
         return new(Dict{VI, CI}(),
-                    Dict{CI, Vector{VI}}(),
-                    Dict{CI, CI}(),
-                    Dict{CI, Vector{T}}())
+                   Dict{CI, Vector{VI}}(),
+                   Dict{CI, CI}(),
+                   Dict{CI, Vector{T}}())
     end
 end
 
-struct DualProblem
-    dual_model::MOI.ModelLike 
-    primal_dual_map::PrimalDualMap
+function is_empty(primal_dual_map::PrimalDualMap{T}) where T
+    if isempty(primal_dual_map.primal_var_dual_con) &&
+       isempty(primal_dual_map.primal_con_dual_var) &&
+       isempty(primal_dual_map.primal_con_dual_con) &&
+       isempty(primal_dual_map.primal_con_constants)
+       return true
+    end
+    return false
+end
+
+function empty!(primal_dual_map::PrimalDualMap{T}) where T
+    primal_dual_map.primal_var_dual_con = Dict{VI, CI}()
+    primal_dual_map.primal_con_dual_var = Dict{CI, Vector{VI}}()
+    primal_dual_map.primal_con_dual_con = Dict{CI, CI}()
+    primal_dual_map.primal_con_constants = Dict{CI, Vector{T}}()
+end
+
+struct DualProblem{T}
+    dual_model::MOI.ModelLike #It can be a model from an optimizer or a DualizableModel{T}
+    primal_dual_map::PrimalDualMap{T}
+
+    # Empty DualProblem cosntructor
+    function DualProblem{T}() where T
+        return new(DualizableModel{T}(), PrimalDualMap{T}())
+    end
+    function DualProblem{T}(dual_optimizer::OT) where {T, OT <: MOI.ModelLike}
+        return new(dual_optimizer, PrimalDualMap{T}())
+    end
+    function DualProblem(dual_optimizer::OT) where {OT <: MOI.ModelLike}
+        return DualProblem{Float64}(MOIU.CachingOptimizer(DualizableModel{Float64}(), dual_optimizer))
+    end
 end
 
 include("utils.jl")
