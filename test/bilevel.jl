60141d6b38392d0abe0e82a3e1f3d9aef780b8f9
Joaquim Garcia <joaquimgarcia@psr-inc.com>
2019-11-08 17:46:05 -0300

    add more complete tests

--- test/bilevel.jl
+++ test/bilevel.jl
@@ -24,297 +24,253 @@ using Gurobi, MathOptFormat, JuMP
 # obj_upper = -8
 # obj_lower =  0
 
-# @testset "Simple LP" begin
-#     optimizer = Gurobi.Optimizer()
-#     bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
-#     MOI.empty!(bridged)
-#     @test MOI.is_empty(bridged)
-
-#     # add 10 variables - only diagonal is relevant
-#     X = MOI.add_variables(bridged, 2)
-
-#     c1 = MOIU.normalize_and_add_constraint(bridged, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(2.0, X[1]),
-#             MOI.ScalarAffineTerm(1.0, X[2])
-#         ], 0.0), MOI.EqualTo(4.0))
-
-#     c2 = MOIU.normalize_and_add_constraint(bridged, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X[1]),
-#             MOI.ScalarAffineTerm(2.0, X[2])
-#         ], 0.0), MOI.EqualTo(4.0))
-
-#     b1 = MOIU.normalize_and_add_constraint(bridged, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X[1])
-#         ], 0.0), MOI.GreaterThan(0.0))
-
-#     b2 = MOIU.normalize_and_add_constraint(bridged, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X[2])
-#         ], 0.0), MOI.GreaterThan(0.0))
-
-#     MOI.set(bridged, 
-#         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
-#         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([-4.0, -3.0], [X[1], X[2]]), 0.0)
-#         )
-#     MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-#     MOI.optimize!(bridged)
-
-#     obj = MOI.get(bridged, MOI.ObjectiveValue())
-
-#     @test obj ≈ -9.33333 atol = 1e-2
-
-#     Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
-
-#     @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
-
-# end
+@testset "Simple LP" begin
+    optimizer = Gurobi.Optimizer()
+    bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
+    MOI.empty!(bridged)
+    @test MOI.is_empty(bridged)
+
+    # add 10 variables - only diagonal is relevant
+    X = MOI.add_variables(bridged, 2)
+
+    c1 = MOIU.normalize_and_add_constraint(bridged, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(2.0, X[1]),
+            MOI.ScalarAffineTerm(1.0, X[2])
+        ], 0.0), MOI.EqualTo(4.0))
+
+    c2 = MOIU.normalize_and_add_constraint(bridged, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X[1]),
+            MOI.ScalarAffineTerm(2.0, X[2])
+        ], 0.0), MOI.EqualTo(4.0))
 
-# @testset "Simple BLP" begin
-#     optimizer = Gurobi.Optimizer()
-#     bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
-#     MOI.empty!(bridged)
-#     @test MOI.is_empty(bridged)
+    b1 = MOIU.normalize_and_add_constraint(bridged, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X[1])
+        ], 0.0), MOI.GreaterThan(0.0))
 
-#     X = MOI.add_variables(bridged, 2)
+    b2 = MOIU.normalize_and_add_constraint(bridged, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X[2])
+        ], 0.0), MOI.GreaterThan(0.0))
 
-#     MOI.set(bridged, MOI.VariableName(), X[1], "x_u")
-#     MOI.set(bridged, MOI.VariableName(), X[2], "y_u")
+    MOI.set(bridged, 
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([-4.0, -3.0], [X[1], X[2]]), 0.0)
+        )
+    MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    MOI.optimize!(bridged)
 
-#     # c1 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(2.0, X[1]),
-#     #         MOI.ScalarAffineTerm(1.0, X[2])
-#     #     ], 0.0), MOI.EqualTo(4.0))
+    obj = MOI.get(bridged, MOI.ObjectiveValue())
 
-#     # c2 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[1]),
-#     #         MOI.ScalarAffineTerm(2.0, X[2])
-#     #     ], 0.0), MOI.EqualTo(4.0))
+    @test obj ≈ -9.33333 atol = 1e-2
 
-#     # b1 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[1])
-#     #     ], 0.0), MOI.GreaterThan(0.0))
+    Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
 
-#     # b2 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[2])
-#     #     ], 0.0), MOI.GreaterThan(0.0))
+    @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
 
-#     MOI.set(bridged, 
-#         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
-#         MOI.ScalarAffineFunction(
-#             MOI.ScalarAffineTerm.([-4.0, -3.0], [X[1], X[2]]), 0.0)
-#         )
-#     MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+end
 
-#     optimizer2 = Gurobi.Optimizer()
-#     bridged2 = MOIB.full_bridge_optimizer(optimizer2, Float64)
-#     MOI.empty!(bridged2)
-#     @test MOI.is_empty(bridged2)
+@testset "Simple BLP" begin
+    optimizer = Gurobi.Optimizer()
+    bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
+    MOI.empty!(bridged)
+    @test MOI.is_empty(bridged)
 
-#     X2 = MOI.add_variables(bridged2, 2)
-#     MOI.set(bridged2, MOI.VariableName(), X2[1], "x_l")
-#     MOI.set(bridged2, MOI.VariableName(), X2[2], "y_l")
+    X = MOI.add_variables(bridged, 2)
 
-
-#     c1 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(2.0, X2[1]),
-#             MOI.ScalarAffineTerm(1.0, X2[2])
-#         ], 0.0), MOI.LessThan(4.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), c1, "l_c1")
+    MOI.set(bridged, MOI.VariableName(), X[1], "x_u")
+    MOI.set(bridged, MOI.VariableName(), X[2], "y_u")
 
-#     c2 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X2[1]),
-#             MOI.ScalarAffineTerm(2.0, X2[2])
-#         ], 0.0), MOI.LessThan(4.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), c2, "l_c2")
+    MOI.set(bridged, 
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([-4.0, -3.0], [X[1], X[2]]), 0.0)
+        )
+    MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
-#     b1 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X2[1])
-#         ], 0.0), MOI.GreaterThan(0.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), b1, "l_b1")
+    optimizer2 = Gurobi.Optimizer()
+    bridged2 = MOIB.full_bridge_optimizer(optimizer2, Float64)
+    MOI.empty!(bridged2)
+    @test MOI.is_empty(bridged2)
 
+    X2 = MOI.add_variables(bridged2, 2)
+    MOI.set(bridged2, MOI.VariableName(), X2[1], "x_l")
+    MOI.set(bridged2, MOI.VariableName(), X2[2], "y_l")
 
-#     b2 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, X2[2])
-#         ], 0.0), MOI.GreaterThan(0.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), b2, "l_b12")
 
-#     MOI.set(bridged2, 
-#         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
-#         MOI.ScalarAffineFunction(
-#             MOI.ScalarAffineTerm.([1.0], [X2[2]]), 0.0)
-#         )
-#     MOI.set(bridged2, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    c1 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(2.0, X2[1]),
+            MOI.ScalarAffineTerm(1.0, X2[2])
+        ], 0.0), MOI.LessThan(4.0))
+    MOI.set(bridged2, MOI.ConstraintName(), c1, "l_c1")
 
+    c2 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X2[1]),
+            MOI.ScalarAffineTerm(2.0, X2[2])
+        ], 0.0), MOI.LessThan(4.0))
+    MOI.set(bridged2, MOI.ConstraintName(), c2, "l_c2")
 
-#     links = Dict(X[1] => X2[1], X[2] => X2[2])
+    b1 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X2[1])
+        ], 0.0), MOI.GreaterThan(0.0))
+    MOI.set(bridged2, MOI.ConstraintName(), b1, "l_b1")
 
-#     parametric = [X2[1]] 
 
-#     blp = Dualization.build_bilivel(bridged, bridged2, links, parametric)
-
+    b2 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, X2[2])
+        ], 0.0), MOI.GreaterThan(0.0))
+    MOI.set(bridged2, MOI.ConstraintName(), b2, "l_b12")
 
-#     # MOI.optimize!(bridged)
-
-#     # obj = MOI.get(bridged, MOI.ObjectiveValue())
-
-#     # @test obj ≈ -9.33333 atol = 1e-2
-
-#     # Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
+    MOI.set(bridged2, 
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0], [X2[2]]), 0.0)
+        )
+    MOI.set(bridged2, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
-#     # @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
-
-#     lp_model = MathOptFormat.MOF.Model()
-#     MOI.copy_to(lp_model, blp)
-#     MOI.write_to_file(lp_model, "my_model.LP")
-#     @show pwd()
-
-#     optimizer = Gurobi.Optimizer()
-#     bridged3 = MOIB.full_bridge_optimizer(optimizer, Float64)
-#     MOI.empty!(bridged3)
-#     @test MOI.is_empty(bridged3)
-#     MOI.copy_to(bridged3, blp)
 
-#     MOI.optimize!(bridged3)
+    links = Dict(X[1] => X2[1], X[2] => X2[2])
 
-# end
+    parametric = [X2[1]] 
 
-# @testset "Simple BLP2" begin
-#     optimizer = Gurobi.Optimizer()
-#     bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
-#     MOI.empty!(bridged)
-#     @test MOI.is_empty(bridged)
+    blp, _, _ = Dualization.build_bilivel(bridged, bridged2, links, parametric)
 
-#     x = MOI.add_variable(bridged)
-#     y = MOI.add_variable(bridged)
 
-#     MOI.set(bridged, MOI.VariableName(), x, "x_u")
-#     MOI.set(bridged, MOI.VariableName(), y, "y_u")
+    # MOI.optimize!(bridged)
 
-#     # c1 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(2.0, X[1]),
-#     #         MOI.ScalarAffineTerm(1.0, X[2])
-#     #     ], 0.0), MOI.EqualTo(4.0))
+    # obj = MOI.get(bridged, MOI.ObjectiveValue())
 
-#     # c2 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[1]),
-#     #         MOI.ScalarAffineTerm(2.0, X[2])
-#     #     ], 0.0), MOI.EqualTo(4.0))
+    # @test obj ≈ -9.33333 atol = 1e-2
 
-#     # b1 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[1])
-#     #     ], 0.0), MOI.GreaterThan(0.0))
+    # Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
 
-#     # b2 = MOIU.normalize_and_add_constraint(bridged, 
-#     #     MOI.ScalarAffineFunction([
-#     #         MOI.ScalarAffineTerm(1.0, X[2])
-#     #     ], 0.0), MOI.GreaterThan(0.0))
+    # @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
 
-#     MOI.set(bridged, 
-#         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
-#         MOI.ScalarAffineFunction(
-#             MOI.ScalarAffineTerm.([1.0, 3.0], [x, y]), 0.0)
-#         )
-#     MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    # lp_model = MathOptFormat.MOF.Model()
+    # MOI.copy_to(lp_model, blp)
+    # MOI.write_to_file(lp_model, "my_model.LP")
+    # @show pwd()
 
-#     optimizer2 = Gurobi.Optimizer()
-#     bridged2 = MOIB.full_bridge_optimizer(optimizer2, Float64)
-#     MOI.empty!(bridged2)
-#     @test MOI.is_empty(bridged2)
+    optimizer = Gurobi.Optimizer()
+    bridged3 = MOIB.full_bridge_optimizer(optimizer, Float64)
+    MOI.empty!(bridged3)
+    @test MOI.is_empty(bridged3)
+    MOI.copy_to(bridged3, blp)
 
-#     x2 = MOI.add_variable(bridged2)
-#     y2 = MOI.add_variable(bridged2)
+    MOI.optimize!(bridged3)
 
-#     MOI.set(bridged2, MOI.VariableName(), x2, "x_l")
-#     MOI.set(bridged2, MOI.VariableName(), y2, "y_l")
+end
 
+@testset "Simple BLP2" begin
+    optimizer = Gurobi.Optimizer()
+    bridged = MOIB.full_bridge_optimizer(optimizer, Float64)
+    MOI.empty!(bridged)
+    @test MOI.is_empty(bridged)
 
-#     c1 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, x2),
-#             MOI.ScalarAffineTerm(1.0, y2)
-#         ], 0.0), MOI.LessThan(8.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), c1, "l_c1")
+    x = MOI.add_variable(bridged)
+    y = MOI.add_variable(bridged)
 
-#     c2 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, x2),
-#             MOI.ScalarAffineTerm(4.0, y2)
-#         ], 0.0), MOI.GreaterThan(8.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), c2, "l_c2")
+    MOI.set(bridged, MOI.VariableName(), x, "x_u")
+    MOI.set(bridged, MOI.VariableName(), y, "y_u")
 
-#     c3 = MOIU.normalize_and_add_constraint(bridged2, 
-#     MOI.ScalarAffineFunction([
-#         MOI.ScalarAffineTerm(1.0, x2),
-#         MOI.ScalarAffineTerm(2.0, y2)
-#     ], 0.0), MOI.LessThan(13.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), c3, "l_c3")
+    MOI.set(bridged, 
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 3.0], [x, y]), 0.0)
+        )
+    MOI.set(bridged, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
-#     b1 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, x2)
-#         ], 0.0), MOI.GreaterThan(1.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), b1, "l_b1")
+    optimizer2 = Gurobi.Optimizer()
+    bridged2 = MOIB.full_bridge_optimizer(optimizer2, Float64)
+    MOI.empty!(bridged2)
+    @test MOI.is_empty(bridged2)
 
+    x2 = MOI.add_variable(bridged2)
+    y2 = MOI.add_variable(bridged2)
 
-#     b2 = MOIU.normalize_and_add_constraint(bridged2, 
-#         MOI.ScalarAffineFunction([
-#             MOI.ScalarAffineTerm(1.0, x2)
-#         ], 0.0), MOI.LessThan(6.0))
-#     MOI.set(bridged2, MOI.ConstraintName(), b2, "l_b12")
+    MOI.set(bridged2, MOI.VariableName(), x2, "x_l")
+    MOI.set(bridged2, MOI.VariableName(), y2, "y_l")
 
-#     MOI.set(bridged2, 
-#         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
-#         MOI.ScalarAffineFunction(
-#             MOI.ScalarAffineTerm.([-1.0], [y2]), 0.0)
-#         )
-#     MOI.set(bridged2, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
+    c1 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, x2),
+            MOI.ScalarAffineTerm(1.0, y2)
+        ], 0.0), MOI.LessThan(8.0))
+    MOI.set(bridged2, MOI.ConstraintName(), c1, "l_c1")
 
-#     links = Dict(x => x2, y => y2)
+    c2 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, x2),
+            MOI.ScalarAffineTerm(4.0, y2)
+        ], 0.0), MOI.GreaterThan(8.0))
+    MOI.set(bridged2, MOI.ConstraintName(), c2, "l_c2")
 
-#     parametric = [x2] 
+    c3 = MOIU.normalize_and_add_constraint(bridged2, 
+    MOI.ScalarAffineFunction([
+        MOI.ScalarAffineTerm(1.0, x2),
+        MOI.ScalarAffineTerm(2.0, y2)
+    ], 0.0), MOI.LessThan(13.0))
+    MOI.set(bridged2, MOI.ConstraintName(), c3, "l_c3")
 
-#     blp = Dualization.build_bilivel(bridged, bridged2, links, parametric)
+    b1 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, x2)
+        ], 0.0), MOI.GreaterThan(1.0))
+    MOI.set(bridged2, MOI.ConstraintName(), b1, "l_b1")
 
 
-#     # MOI.optimize!(bridged)
+    b2 = MOIU.normalize_and_add_constraint(bridged2, 
+        MOI.ScalarAffineFunction([
+            MOI.ScalarAffineTerm(1.0, x2)
+        ], 0.0), MOI.LessThan(6.0))
+    MOI.set(bridged2, MOI.ConstraintName(), b2, "l_b12")
 
-#     # obj = MOI.get(bridged, MOI.ObjectiveValue())
+    MOI.set(bridged2, 
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), 
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([-1.0], [y2]), 0.0)
+        )
+    MOI.set(bridged2, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
-#     # @test obj ≈ -9.33333 atol = 1e-2
 
-#     # Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
+    links = Dict(x => x2, y => y2)
 
-#     # @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
+    parametric = [x2] 
 
-#     lp_model = MathOptFormat.MOF.Model()
-#     MOI.copy_to(lp_model, blp)
-#     MOI.write_to_file(lp_model, "my_model.LP")
-#     @show pwd()
+    blp, _, _ = Dualization.build_bilivel(bridged, bridged2, links, parametric)
 
-#     optimizer = Gurobi.Optimizer()
-#     bridged3 = MOIB.full_bridge_optimizer(optimizer, Float64)
-#     MOI.empty!(bridged3)
-#     @test MOI.is_empty(bridged3)
-#     MOI.copy_to(bridged3, blp)
 
-#     MOI.optimize!(bridged3)
+    # MOI.optimize!(bridged)
 
-# end
+    # obj = MOI.get(bridged, MOI.ObjectiveValue())
+
+    # @test obj ≈ -9.33333 atol = 1e-2
+
+    # Xr = MOI.get(bridged, MOI.VariablePrimal(), X)
+
+    # @test Xr ≈ [1.3333, 1.3333] atol = 1e-2
+
+    # lp_model = MathOptFormat.MOF.Model()
+    # MOI.copy_to(lp_model, blp)
+    # MOI.write_to_file(lp_model, "my_model.LP")
+    # @show pwd()
+
+    optimizer = Gurobi.Optimizer()
+    bridged3 = MOIB.full_bridge_optimizer(optimizer, Float64)
+    MOI.empty!(bridged3)
+    @test MOI.is_empty(bridged3)
+    MOI.copy_to(bridged3, blp)
+
+    MOI.optimize!(bridged3)
+
+end
 
 @testset "Simple BLP JuMP" begin
 
@@ -338,10 +294,41 @@ using Gurobi, MathOptFormat, JuMP
 
     termination_status(model)
 
-    objective_value(model)
+    @test objective_value(model) == -8
 
-    value(x)
-    value(y)
+    @test value(x) ==  2
+    @test value(y) ==  0
+
+end
+
+@testset "Simple BLP2 JuMP" begin
+
+    model = BilevelModel()
+
+    @variable(UpperToLower(model), x)
+    @variable(LowerToUpper(model), y)
+
+    @objective(Upper(model), Min, x + 3y)
+
+    @objective(Lower(model), Min, -y)
+
+    @constraint(Lower(model), x+y <= 8)
+    @constraint(Lower(model), x+4y >= 8)
+    @constraint(Lower(model), x+2y <= 13)
+    @constraint(Lower(model), x >= 1)
+    @constraint(Lower(model), x <= 6)
+    @constraint(Lower(model), y >= 0)
+
+    optimize!(model, Gurobi.Optimizer())
+
+    primal_status(model)
+
+    termination_status(model)
+
+    @test objective_value(model) == 12
+
+    @test value(x) == 6
+    @test value(y) == 2
 
 end
 
