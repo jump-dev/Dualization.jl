a9f922fb17f1f7296917163da9be5f76a97e29f0
guilhermebodin <guilherme.b.moraes@gmail.com>
2022-01-06 18:40:09 -0300

    Remove ECOS Conic Linear problems

--- test/Solvers/ecos_test.jl
+++ test/Solvers/ecos_test.jl
@@ -5,14 +5,8 @@ const ECOS_DUAL_FACTORY = dual_optimizer(ECOS_PRIMAL_FACTORY)
 const ECOS_PRIMAL_OPT = MOI.instantiate(ECOS_PRIMAL_FACTORY)
 const ECOS_DUAL_OPT = MOI.instantiate(ECOS_DUAL_FACTORY)
 
-# push!(dual_linear_optimizer, DualOptimizer(ECOS.Optimizer(verbose = 0)))
 
 @testset "ECOS conic Problems" begin
-    @testset "ECOS Conic linear problems" begin
-        list_of_soc_problems =
-            [conic_linear2_test, conic_linear3_test, conic_linear4_test]
-        test_strong_duality(list_of_soc_problems, ECOS_PRIMAL_FACTORY)
-    end
     @testset "ECOS SOC problems" begin
         list_of_soc_problems =
             [soc1_test, soc2_test, soc3_test, soc4_test, soc5_test, soc6_test]
