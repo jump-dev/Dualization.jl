73e101ced06a6133d18e7375b1e3b3a76faeaa08
Oscar Dowson <odow@users.noreply.github.com>
2024-12-19 09:03:05 +1300

    Update src/MOI_wrapper.jl

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -5,7 +5,10 @@
 
 export DualOptimizer, dual_optimizer
 
-function dual_optimizer(optimizer_constructor; coefficients_type::Type{T} = Float64) where {T <: Number}
+function dual_optimizer(
+    optimizer_constructor;
+    coefficients_type::Type{T} = Float64,
+) where {T<:Number}
     return () -> DualOptimizer{T}(MOI.instantiate(optimizer_constructor))
 end
 
