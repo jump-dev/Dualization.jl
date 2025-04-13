114fe2df2b14d7f8a4ae9e1cfd9d0ad8f7e5369e
joaquimg <joaquimdgarcia@gmail.com>
2025-04-12 22:23:51 -0700

    rm hashtags

--- src/structures.jl
+++ src/structures.jl
@@ -37,7 +37,7 @@ Main user maps:
     constraints (the special ones that makes them constrained variables) and
     their internal index from 1 to dimension(set) (if vector constraints:
     VectorOfVariables-in-Set), 1 otherwise (scalar: VariableIndex-in-Set).
-    # primal_convar_to_primal_convarcon_and_index
+    Future name: primal_convar_to_primal_convarcon_and_index
 
   * `constrained_var_dual::Dict{MOI.ConstraintIndex,MOI.ConstraintIndex}`: maps
     the primal constraint index of constrained variables to the dual
@@ -45,8 +45,8 @@ Main user maps:
     constraint is a regular constraint (not a constrained variable constraint).
     `VectorOfVariables`-in-`Zeros` and `VariableIndex`-in-`EqualTo(zero(T))`
     are not in this map, as they are not dualized (See
-    # primal_convarcon_to_dual_function).
-    # primal_convarcon_to_dual_con
+    primal_convarcon_to_dual_function).
+    Future name: primal_convarcon_to_dual_con
 
     note: from the above two maps, we can get primal_convar_to_dual_con_and_index
 
@@ -55,7 +55,7 @@ Main user maps:
     Free variables as opposed to constrained variables. Note that Dualization
     will select automatically which variables are free and which are
     constrained.
-    # primal_var_to_dual_con
+    Future name: primal_var_to_dual_con
 
     note: from the above three maps, we can get primal_var_to_dual_con_and_index
 
@@ -64,7 +64,7 @@ Main user maps:
     scalar constraints those vectors will be single element vectors.
     Primal Constrained variables constraints (the main ones) are not in this
     map.
-    # primal_con_to_dual_var_vec
+    Future name: primal_con_to_dual_var_vec
 
   * `primal_con_dual_con::Dict{MOI.ConstraintIndex,MOI.ConstraintIndex}`: maps
     primal constraints to dual constrained variable. If the primal
@@ -73,7 +73,7 @@ Main user maps:
     The keys are similar to the (# primal_con_to_dual_var_vec) map, except
     that `VariableIndex`-in-`EqualTo(zero(T))` and `VectorOfVariables`-in-`Zeros`
     are not in this map.
-    # primal_con_to_dual_convarcon
+    Future name: primal_con_to_dual_convarcon
 
   Additional helper maps:
 
@@ -82,11 +82,11 @@ Main user maps:
     This map is used in `MOI.get(::DualOptimizer,::MOI.ConstraintPrimal,ci)`
     that requires extra information in the case that the scalar set constrains
     a constant (`EqualtTo`, `GreaterThan`, `LessThan`).
-    # primal_con_to_primal_constants_vec
+    Future name: primal_con_to_primal_constants_vec
 
   * `primal_parameter::Dict{MOI.VariableIndex,MOI.VariableIndex}`: maps
     parameters in the primal to parameters in the dual model.
-    # primal_parameter_to_dual_parameter
+    Future name: primal_parameter_to_dual_parameter
 
   * `constrained_var_zero::Dict{MOI.ConstraintIndex,Unions{MOI.ScalarAffineFunction,MOI.VectorAffineFunction}}`:
     caches scalar affine functions or vector affine functions associated with
@@ -94,12 +94,12 @@ Main user maps:
     `VariableIndex`-in-`EqualTo(zero(T))` as their duals would be `func`-in-`Reals`,
     which are "irrelevant" to the model. This information is cached for
     completeness of the `DualOptimizer` for `get`ting `ConstraintDuals`.
-    # primal_convarcon_to_dual_function
+    Future name: primal_convarcon_to_dual_function
 
   * `primal_var_dual_quad_slack::Dict{MOI.VariableIndex,MOI.VariableIndex}`:
     maps primal variables (that appear in quadratic objective terms) to dual
     "slack" variables. These primal variables might appear in other maps.
-    # primal_var_in_quad_obj_to_dual_slack_var
+    Future name: primal_var_in_quad_obj_to_dual_slack_var
 """
 mutable struct PrimalDualMap{T}
     constrained_var_idx::Dict{MOI.VariableIndex,Tuple{MOI.ConstraintIndex,Int}}
