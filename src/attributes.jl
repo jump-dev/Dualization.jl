9c8243137248ddbbf85a330d5628695ede9ee080
Benoît Legat <benoit.legat@gmail.com>
2026-04-16 18:15:53 +0200

    Fix set/get for RawStatusString

--- src/attributes.jl
+++ src/attributes.jl
@@ -28,6 +28,7 @@ model with the dual model.
 function dual_attribute end
 
 dual_attribute(attr::MOI.ResultCount) = attr
+dual_attribute(attr::MOI.RawStatusString) = attr
 
 dual_attribute(::MOI.VariableName) = MOI.ConstraintName()
 
